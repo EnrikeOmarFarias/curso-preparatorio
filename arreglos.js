18c31ca9b9f4bbb2a0783138301814f2d76430b0
enrikefarias <enrikefarias@gmail.com>
2023-12-09 17:56:59 -0300

    array

--- arreglos.js
+++ arreglos.js
@@ -1,9 +1,9 @@
 var colores = ["rojo", "blanco"];
 
-colores.push("Azul");
-colores.unshift("verde");
-colores.pop();
-colores.shift();
+colores.push("Azul"); // Agregar elemento al final del arreglo
+colores.unshift("verde"); // Agregar elementos al principio del arreglo
+colores.pop(); // Elimina el ultimo elemento
+colores.shift(); // Elimina el ultimo elemento
 console.log(colores);
 
 var incluye_rojo = colores.includes("roja");
@@ -14,3 +14,29 @@ var cumplen_condicion = numeros.every((num) => {
   return num > 5;
 });
 console.log(cumplen_condicion);
+
+var listaDECompras = [];
+listaDECompras[3] = "tomates";
+console.log(listaDECompras[3]);
+
+listaDECompras[1] = "Lechuga";
+var elementosDElArray = listaDECompras[1];
+console.log(elementosDElArray.length);
+
+console.log(listaDECompras.length);
+
+listaDECompras.push("Melon");
+listaDECompras.unshift("Sandia");
+listaDECompras.pop();
+listaDECompras.shift();
+
+console.log(listaDECompras);
+
+var jugadores = ["Orteguita", "Enzo", "Aymar"];
+var incluyeEnzo = jugadores.includes("Enzo");
+console.log(incluyeEnzo);
+
+var numerosPares = [2, 4, 6, 8];
+numerosPares.every((num) => {
+  num / 2 == 0;
+});
